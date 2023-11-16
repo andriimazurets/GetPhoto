e72717af223488d7aa0947f927af5344360ee46a
theri <therigasmazurets@gmail.com>
2023-11-16 18:06:27 +0200

    add menu toolbar

--- app/src/main/java/com/example/getphoto/MainActivity.kt
+++ app/src/main/java/com/example/getphoto/MainActivity.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.getphoto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,5 +31,21 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == 0)
             binding.mainImage.setImageURI(data?.data)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miAddContact -> Toast.makeText(this, "You clicked on Add Contact", Toast.LENGTH_LONG).show()
+            R.id.miFavorites -> Toast.makeText(this, "You clicked on Favorites", Toast.LENGTH_LONG).show()
+            R.id.miSettings -> Toast.makeText(this, "You clicked on Settings", Toast.LENGTH_LONG).show()
+            R.id.miFeedback -> Toast.makeText(this, "You clicked on Feedback", Toast.LENGTH_LONG).show()
+            R.id.miClose -> finish()
+        }
+        return true
     }
 }
